1a8df4828b94f8a75daf0492c24a6955cbd16987
Balamoote <vetall@monte.monte.org.ua>
2023-06-12 21:24:42 +0300

    up

--- tools/scriptdb/hclean.sh
+++ tools/scriptdb/hclean.sh
@@ -7,30 +7,34 @@ key="$1"
 
 case $key in
     -ord )
-       awk '{
-             reg = "(^.* )[drvDRV](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
+       awk '{    reg = "(^.* )[drvDRVZ](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
              if ($0 ~ reg) {
                  rule++
                  match($0,reg,rega);
                  $0 = rega[1] "R" rega[2] rule rega[3] "R" rule rega[4]
                  print $0 } else { print $0 };
             }' deomo.awk | awk -f sort_cst.awk > deomo.awk_ord; mv deomo.awk_ord deomo.awk;
-       awk '{
-             reg = "(^.* )[drvDRV](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
+       awk '{    reg = "(^.* )[drvDRV](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
              if ($0 ~ reg) {
                  rule++
                  match($0,reg,rega);
                  $0 = rega[1] "V" rega[2] rule rega[3] "V" rule rega[4]
                  print $0 } else { print $0 };
             }' vsevso.awk | awk -f sort_cst.awk > vsevso.awk_ord; mv vsevso.awk_ord vsevso.awk;
-       awk '{
-             reg = "(^.* )[drvDRV](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
+       awk '{    reg = "(^.* )[drvDRVZ](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRV][0-9]+(\x22.*)$"
              if ($0 ~ reg) {
                  rule++
                  match($0,reg,rega);
                  $0 = rega[1] "D" rega[2] rule rega[3] "D" rule rega[4]
                  print $0 } else { print $0 };
             }' defunct.awk | awk -f sort_cst.awk > defunct.awk_ord; mv defunct.awk_ord defunct.awk;
+       awk '{    reg = "(^.* )[drvDRVZ](\\[)[0-9]+(\\]\\+\\+; if\\(dbg\\){print \x22)[DRVZ][0-9]+(\x22.*)$"
+             if ($0 ~ reg) {
+                 rule++
+                 match($0,reg,rega);
+                 $0 = rega[1] "Z" rega[2] rule rega[3] "Z" rule rega[4]
+                 print $0 } else { print $0 };
+            }' zamok.awk | awk -f sort_cst.awk > zamok.awk_ord; mv zamok.awk_ord zamok.awk;
         exit 1; ;;
 
     * ) exit 0; ;;
