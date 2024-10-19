11816a38ae7c55db8dcbb439f5f02347787e4f36
vetall <vetall@montealba.montealba.org>
2024-10-19 16:38:53 +0300

    up

--- tools/scriptdb/ruac.py
+++ tools/scriptdb/ruac.py
@@ -1,3 +1,12 @@
+# Оригинал пакета ruaccent тут: https://github.com/Den4ikAI/ruaccent
+# Свежая версия данного скрипта тут: https://github.com/Balamoote/gtts-scripts/tools/scriptdb/ruac.py
+
+# Для более корректной работы, найдите файл ruaccent.py, поменяйте и дополните так:
+# self_normalize_seq="[^a-zA-Z0-9\sа-яА-ЯёЁ—–.,!?:;\"'‘’(){}\[\]«»„“”\\\`_~@#$%^&*=/-]"
+# self.normalize = re.compile(fr"{self_normalize_seq}")
+
+# Пример запуска: python ruac.py -cuda book.txt > book_acc.txt
+
 import gzip
 import os
 import re
@@ -5,41 +14,64 @@ import sys
 import time
 import unicodedata
 
+import onnxruntime as ort  # нужен только для выставления уровня логов
 from ruaccent import RUAccent
-
-# ruaccent.py поменять и дополнить:
-# self_normalize_seq="[^a-zA-Z0-9\sа-яА-ЯёЁ—–.,!?:;""'‘’(){}\[\]«»„“”\\\`_~@#$%^&*=/-]"
-# self.normalize = re.compile(fr"{self_normalize_seq}")
 
 # Начало отсчета времени
 start_time = time.time()
 
-# Параметры по умолчанию
-use_dictionary = True
-custom_dict = {}
-device = "CPU"
-model_name = 'turbo3.1'
-max_length = 1024  # размер чанка
-seps_priority = ['. ', '! ', '? ', '... ', '; ', ': ', ', ', ' ']  # приоритет сепараторов для чанков
-show_status = 1  # вывод статуса при обработке файла (выключен для stdin)
-plused = 0       # 0 = ста́вить ударе́ния, 1 = ст+авить удар+ения
-save_accent = 1  # восстанавливать ударения из исходного текста
-see_no_error = 1 # если включено, то ошибочные слова восстанавливаются из оригинала и сама ошибка не будет видна в файле errata.txt
-cpus = "32"      # Установите это значение в соответствии с вашим оборудованием
+# Параметры ruaccent по умолчанию
+use_dictionary = True       # Использовать встроенный словарь однозначных ударений, выключается по -nodic
+custom_dict    = {}         # подгрузить пользовательский словарь однозначных ударений: -dic
+device         = "CUDA"      # CPU или CUDA
+model_name     = 'turbo3.1' # название модели
+max_length     = 1024       # размер чанка
 
-pat_plus = "[^а-яА-ЯёЁ+]+"
-pat_acc = "[^а-яА-ЯёЁ\u0301]+"
+# Параметры скрипта по умолчанию
+seps_priority  = ['. ', '! ', '? ', '... ', '; ', ': ', ', ', ' ']  # приоритет сепараторов для чанков
+show_status    = 1          # вывод статуса при обработке файла (выключен для stdin), выключить: -nostat или -norep
+final_report   = 1          # вывод финального отчета, выключить: -norep
+plused         = 0          # 0 = ста́вить ударе́ния, 1 = ст+авить удар+ения, включить: -plus
+save_accent    = 1          # восстанавливать уже имеющиеся ударения из исходного текста, выключить: -nosave
+see_no_error   = 1          # скрыть дефектные результаты обработки в errata.txt; в конечной строке дефекты устраняются всегда, включить: -errors
+cpus           = "32"       # Установите это значение в соответствии с вашим оборудованием
+onnx_log_lvl   = 3          # Настройка лога onnx, может быть нужна, если используем CUDA
 
 # ANSI escape codes для цветного вывода
-GREEN = '\033[92m'
+GREEN  = '\033[92m'
 YELLOW = '\033[93m'
-RESET = '\033[0m'
+RESET  = '\033[0m'
+
 os.environ['OMP_NUM_THREADS'] = cpus
 os.environ['MKL_NUM_THREADS'] = cpus
+ort.set_default_logger_severity(onnx_log_lvl)
+
+# Паттерны разделителей
+pat_plus = re.compile("[^а-яА-ЯёЁ+]+")
+pat_plus_start = re.compile("^[^а-яА-ЯёЁ+]+")
+pat_acc = re.compile("[^а-яА-ЯёЁ\u0301]+")
+
+# Базовая предварительная чистка
+pre_clean = re.compile(r'<[^<>]+>|…')
+
+# Паттетрны для convert_accent
+patterns = {
+    "apostrophe_to_plus": (re.compile(r"([аеёиоуыэюяАЕЁИОУЫЭЮЯ])'"), lambda m: f"+{m.group(1)}"),
+    "accent_to_plus": (re.compile(r"([аеёиоуыэюяАЕЁИОУЫЭЮЯ])\u0301"), lambda m: f"+{m.group(1)}"),
+    "plus_to_accent": (re.compile(r"\+([аеёиоуыэюяАЕЁИОУЫЭЮЯ])"), lambda m: f"{m.group(1)}\u0301")
+}
+
+# Универсальная функция для преобразования ударений
+def convert_accent(text, conversion_type):
+    if conversion_type in patterns:
+        pattern, replacement = patterns[conversion_type]
+        return pattern.sub(replacement, text)
+
+    return text
 
 # Функция для загрузки custom_dict из файлов dic.gz со строками вида _слово=сло'во
 def load_custom_dict(*file_paths):
-    custom_dict = {}
+#   custom_dict = {}
     for file_path in file_paths:
         with gzip.open(file_path, 'rt', encoding='utf-8') as f:
             for lin in f:
@@ -51,24 +83,10 @@ def load_custom_dict(*file_paths):
                         custom_dict[wrd] = convert_accent(accented_word, "apostrophe_to_plus")
     return custom_dict
 
-# Универсальная функция для преобразования ударений
-def convert_accent(text, conversion_type):
-    patterns = {
-        "apostrophe_to_plus": (r"([аеёиоуыэюяАЕЁИОУЫЭЮЯ])'", r"+\1"),
-        "accent_to_plus": (r"([аеёиоуыэюяАЕЁИОУЫЭЮЯ])\u0301", r"+\1"),
-        "plus_to_accent": (r"\+([аеёиоуыэюяАЕЁИОУЫЭЮЯ])", r"\1" + "\u0301")
-    }
-
-    if conversion_type in patterns:
-        pattern, replacement = patterns[conversion_type]
-        return re.sub(pattern, replacement, text)
-
-    return text
-
 # Функция patsplit для разделения строки на слова и сепараторы
 def patsplit(string, seps_pattern):
-    words = re.split(seps_pattern, string)
-    seps = re.findall(seps_pattern, string)
+    words = seps_pattern.split(string)
+    seps = seps_pattern.findall(string)
     if words and len(words[0]) == 0:
         del words[0]
 
@@ -103,25 +121,42 @@ def split_long_line(lin):
     return parts
 
 def clean_word(wrd):
-    # Убираем ударение (комбинирующий символ \u0301) и заменяем ё на е, Ё на Е
-    word_cleaned = wrd.replace('ё', 'е').replace('Ё', 'Е')
-    # Убираем комбинирующие символы (ударения)
+    word_cleaned = wrd.replace('ё', 'е').replace('Ё', 'Е').replace('+','')
     word_cleaned = ''.join([char for char in unicodedata.normalize('NFD', word_cleaned) if unicodedata.category(char) != 'Mn'])
     return word_cleaned
 
 # Обработка аргументов командной строки
+keys_list = []
 if '-nodic' in sys.argv:
+    keys_list.append('-nodic')
     use_dictionary = False
 if '-cust' in sys.argv:
+    keys_list.append('-cust')
     cust_index = sys.argv.index('-cust') + 1
     if cust_index < len(sys.argv):
         custom_dict = load_custom_dict(sys.argv[cust_index])
-if '-gpu' in sys.argv:
+if '-cuda' in sys.argv:
+    keys_list.append('-cuda')
     device = "CUDA"
+if '-cpu' in sys.argv:
+    keys_list.append('-cpu')
+    device = "CPU"
 if '-plus' in sys.argv:
+    keys_list.append('-plus')
     plused = 1
 if '-nosave' in sys.argv:
+    keys_list.append('-nosave')
     save_accent = 0
+if '-errors' in sys.argv:
+    keys_list.append('-errors')
+    see_no_error = 0
+if '-nostat' in sys.argv:
+    keys_list.append('-nostat')
+    show_status = 0
+if '-norep' in sys.argv:
+    keys_list.append('-norep')
+    show_status = 0
+    final_report = 0
 
 # Удаление файла errata.txt перед началом обработки
 errata_file_path = "errata.txt"
@@ -129,13 +164,18 @@ if os.path.exists(errata_file_path):
     os.remove(errata_file_path)
 
 # Инициализация модели с custom_dict
-line_pattern = pat_plus if plused == 1 else pat_acc
+if plused:
+    line_pattern = pat_plus
+    acc_seq = re.compile(r"\+")
+else:
+    line_pattern = pat_acc
+    acc_seq = re.compile(r"\u0301")
 
 accentizer = RUAccent()
 accentizer.load(omograph_model_size=model_name, use_dictionary=use_dictionary, custom_dict=custom_dict, device=device, tiny_mode=False)
 
 # Чтение текста из файла или stdin
-if len(sys.argv) > 1 and sys.argv[-1] not in ['-dic', '-nodic', '-cust', '-cpu', '-gpu', '-plus', '-nosave']:
+if len(sys.argv) > 1 and sys.argv[-1] not in keys_list:
     filename = sys.argv[-1]
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -147,33 +187,29 @@ total_lines = len(lines)
 
 # Обработка текста построчно
 for line_num, line in enumerate(lines, start=1):
-    line = line.strip()
-#   line = convert_accent(line, "plus_to_accent") # Оверкил
-    if plused == 1:
-        line = convert_accent(line, "accent_to_plus")
-        words_orig, seps_orig = patsplit(line, pat_plus)
-        acc_seq = "\+"
+    line_in = line.rstrip()
+#   line_in = convert_accent(line_in, "plus_to_accent") # Оверкил
+    if plused:
+        line_in = convert_accent(line_in, "accent_to_plus").lstrip()
+        words_orig, seps_orig = patsplit(line_in, pat_plus)
     else:
-        words_orig, seps_orig = patsplit(line, pat_acc)
-        line = convert_accent(line, "accent_to_plus")
-        acc_seq = "\u0301"
+        words_orig, seps_orig = patsplit(line_in, pat_acc)
+        line_in = convert_accent(line_in, "accent_to_plus").lstrip()
 
     # Чистка тэтов и замены…
-    line = re.sub(r'<[^>]+>|…', lambda m: '...' if m.group(0) == '…' else '', line)
+    line_in = pre_clean.sub(lambda m: '...' if m.group(0) == '…' else '', line_in)
 
-    chunks = split_long_line(line)
+    chunks = split_long_line(line_in)
 
     # Отправляем текст на обработку
     proc_chunks = [accentizer.process_all(chunk) for chunk in chunks]
 
     # Собираем строку для повторного разбиения
     proc_line = ' '.join(proc_chunks)
-    # Чистим от всего, оставляя только слова
-    proc_line = re.sub(f'^{pat_plus}', ' ', proc_line)
 
     # Преобразование акцентов обратно
-    if plused != 1:
-        proc_line = convert_accent(proc_line.strip(), "plus_to_accent")
+    if not plused:
+        proc_line = convert_accent(proc_line, "plus_to_accent")
 
     proc_words, proc_seps = patsplit(proc_line, line_pattern)
 
@@ -183,11 +219,18 @@ for line_num, line in enumerate(lines, start=1):
     if len(seps_orig) - len(proc_words) == -1:
         seps_orig.insert(0, '')
 
-    # Восстановление исходных ударений из первичной строки
+    # Восстановление исходных ударений из первичной строки и усиранением ошибок обработки
+    if save_accent:
+        if any(acc_seq.search(word) for word in words_orig):
+            for i, word in enumerate(words_orig):
+                if acc_seq.search(word):
+                    proc_words[i] = word
+
+    # Устранение дефектных результатов обработки
     line_error = 0
-    if save_accent == 1:
+    if clean_word(' '.join(words_orig)) != clean_word(' '.join(proc_words)):
         for i, word in enumerate(words_orig):
-            # Очищаем оригинальное слово
+            line_error = 1
             clean_orig_word = clean_word(word)
             if i < len(proc_words):
                 clean_proc_word = clean_word(proc_words[i])
@@ -197,25 +240,18 @@ for line_num, line in enumerate(lines, start=1):
 
             # Сравниваем очищенные слова
             if clean_orig_word != clean_proc_word:
-                line_error = 1
-                if see_no_error:
-                    proc_words[i] = word
-
-            # Если исходное слово содержит ударение, восстанавливаем его в обработанном тексте
-            if re.search(fr"{acc_seq}", word):
                 proc_words[i] = word
 
     final_line = ''.join([sep + word for sep, word in zip(seps_orig, proc_words)])
 
     if line_error:
         with open(errata_file_path, "a", encoding="utf-8") as errata_file:
-            errata_file.write(f"<line: {line_num}> {line}\n")
+            errata_file.write(f"<line: {line_num}> {line}")
             if not see_no_error:
-                errata_file.write(f"<out : {line_num}> {final_line}\n")
+                errata_file.write(f"<out : {line_num}> {final_line}\n{'-' * 20}\n")
 
     # Вывод строки
     sys.stdout.write(final_line + '\n')
-    sys.stdout.flush()
 
     # Статус обработки -- только при обработке файла, но не потока
     if show_status:
@@ -226,8 +262,10 @@ for line_num, line in enumerate(lines, start=1):
         sys.stderr.flush()
 
 # Финальная строка после завершения обработки
-elapsed_time = time.time() - start_time
-sys.stderr.write(f"\r"
-                 f"\r{GREEN}Запуск:{RESET} {YELLOW}{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}{RESET} "
-                 f"{GREEN}Обработка:{RESET} {YELLOW}{int(elapsed_time // 3600):02}:{int((elapsed_time % 3600) // 60):02}:{int(elapsed_time % 60):02}{RESET} "
-                 f"{GREEN}Всего строк:{RESET} {YELLOW}{line_num}{RESET}".ljust(80) + "\n")
+if final_report:
+    elapsed_time = time.time() - start_time
+    sys.stderr.write(
+        f"\r"
+        f"\r{GREEN}Запуск:{RESET} {YELLOW}{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}{RESET} "
+        f"{GREEN}Обработка:{RESET} {YELLOW}{int(elapsed_time // 3600):02}:{int((elapsed_time % 3600) // 60):02}:{int(elapsed_time % 60):02}{RESET} "
+        f"{GREEN}Всего строк:{RESET} {YELLOW}{line_num}{RESET}".ljust(80) + "\n")
